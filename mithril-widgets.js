5cca2d791875f2f3b968728e10cdb698ed39c461
Nando Florestan <nandoflorestan@gmail.com>
2018-03-24 10:34:49 +0100

    Add deepValue(). Sort supports structured key

--- mithril-widgets.js
+++ mithril-widgets.js
@@ -7,9 +7,9 @@ Object.defineProperty(Object.prototype, 'extend', {
 	configurable: false,
 	enumerable: false,
 	value: function (src) {
+		// Copy variables of *src* into this.
 		if (src === null) return this;
-		const self = this;
-		for (const key of Object.keys(src))  self[key] = src[key];
+		for (const key of Object.keys(src))  this[key] = src[key];
 		return this;
 	}
 });
@@ -18,7 +18,7 @@ Object.defineProperty(Object.prototype, 'pop', {
 	writable: false,
 	configurable: false,
 	enumerable: false,
-	value: function (key) {
+	value: function (key) {  // Return the value and remove *key* from this
 		var value = this[key];
 		delete this[key];
 		return value;
@@ -29,20 +29,36 @@ Object.defineProperty(Array.prototype, 'contains', {value: function (o) {
 	return this.indexOf(o) != -1;
 }});
 
+Object.defineProperty(Object.prototype, 'deepValue', {
+	writable: false,
+	configurable: false,
+	enumerable: false,
+	value: function (key) { // Example key: "person.address.street"
+		// Traverse this object to return the value of a structured key
+		const keyParts = key.split('\.'); // split *key* by dot
+		let val = this;
+		for (const k of keyParts)  {
+			val = val[k];
+			if (val === undefined) return undefined;
+		}
+		return val;
+	}
+});
+
 Array.prototype.sortBy = function (key, desc) {
 	return this.sort(function(a, b) {
-		var x = a[key];
-		var y = b[key];
-		if (typeof x === "string") {
-			x = x.toLowerCase();
+		let va = a.deepValue(key);
+		let vb = b.deepValue(key);
+		if (typeof va === "string") {
+			va = va.toLowerCase();
 		}
-		if (typeof y === "string") {
-			y = y.toLowerCase();
+		if (typeof vb === "string") {
+			vb = vb.toLowerCase();
 		}
 		if (desc) {
-			return (x > y) ? -1 : ((x < y) ? 1 : 0);
+			return (va > vb) ? -1 : ((va < vb) ? 1 : 0);
 		} else {
-			return (x < y) ? -1 : ((x > y) ? 1 : 0);
+			return (va < vb) ? -1 : ((va > vb) ? 1 : 0);
 		}
 	});
 };
