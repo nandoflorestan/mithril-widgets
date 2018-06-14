ca42782c40c69c82762da2aa6beb533e750b6d72
Nando Florestan <nandoflorestan@gmail.com>
2018-06-14 19:03:53 +0200

    Add "use strict", documentation. Indent

--- alert.js
+++ alert.js
@@ -1,4 +1,7 @@
-class Alert {
+"use strict";
+
+
+class Alert {  // A bootstrap 4 popover. *message* can be a Mithril component.
 	constructor({elementId, title, message}) {
 		this.showing = true;
 		this.title = title;
@@ -12,27 +15,27 @@ class Alert {
 
 	view(vnode) {
 		const self = vnode.tag;
-		return m('.dv-alert',
-					m('.card-deck.dv-alert-box',
-						{style: self.showing ?
-							'display:inline-block;' :
-							'display:none;'
-						},
-						m('.card.nice-shadow',
-						[
-							m('.card-header.text-left.bg-info.text-white', self.title),
-							m('.card-body.text-left', self.message),
-							m('div.text-center',
-								m('button.btn.btn-primary.btn-sm',
-									{
-										onclick: () => self.close(),
-										style: 'min-width: 5em; margin: 1em;',
-									},
-									' Ok'
-								)
-							)
-						])
+		return m(
+			'.dv-alert',
+			m('.card-deck.dv-alert-box',
+				{style: self.showing ?
+					'display:inline-block;' :
+					'display:none;'
+				},
+				m('.card.nice-shadow', [
+					m('.card-header.text-left.bg-info.text-white', self.title),
+					m('.card-body.text-left', self.message),
+					m('div.text-center',
+						m('button.btn.btn-primary.btn-sm',
+							{
+								onclick: () => self.close(),
+								style: 'min-width: 5em; margin: 1em;',
+							},
+							' Ok'
+						)
 					)
-				);
+				])
+			)
+		);
 	}
 }
