3d3d2f2b87e9e6f44b6c9d979000a0cd2293b40f
André Maia <pitymaia@gmail.com>
2018-12-20 01:26:56 -0200

    Make _Popover component works together with other components

--- popover.js
+++ popover.js
@@ -1,15 +1,14 @@
 "use strict";
 
 // A bootstrap 4 popover. *message* can be a Mithril component.
-class Popover {  // jshint ignore:line
+class _Popover {  // jshint ignore:line
 	constructor() {
 		this.showing = false;
 	}
 
-	message({title, message}) {
-		this.message = message;
-		this.title = title;
-		this.toggle();
+	oninit(vnode) {
+		// Add this _popover to user class
+		vnode.attrs.ctx._popover = vnode.state;
 	}
 
 	toggle() {
@@ -17,13 +16,12 @@ class Popover {  // jshint ignore:line
 	}
 
 	view(vnode) {
-		const self = vnode.tag;
-		return self.showing ? m(
+		return this.showing ? m(
 			'.mw-popover',
 			[m('.card-deck.mw-popover-box',
 				[m('.card.mw-shadow', [
-					m('.card-header.text-left.bg-info.text-white', self.title),
-					m('.card-body.text-left', [self.message])
+					m('.card-header.text-left.bg-info.text-white', [vnode.attrs.title]),
+					m('.card-body.text-left', [vnode.attrs.message])
 				])]
 			)]
 		) : undefined;
