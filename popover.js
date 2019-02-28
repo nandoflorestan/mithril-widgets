88b6c0c2f002bfe0ca175e2c27ac74d3fefc3086
André Maia <pitymaia@gmail.com>
2019-02-28 20:40:41 -0300

    Close popover when click outside

--- popover.js
+++ popover.js
@@ -18,13 +18,41 @@ class Popover {  // jshint ignore:line
 
 	toggle() {
 		this.showing = !this.showing;
+
+		if (this.showing) {
+			// Add the listener to capture when click outside
+			// First add it to this._ifClickOutside so the function have the same signature when remove the listner
+			this._ifClickOutside = this.ifClickOutside.bind(this);
+			// SetTimeout prevents the first click from being captured if the toggle is called by the click of a button
+			setTimeout(()=> document.addEventListener("click", this._ifClickOutside));
+		} else {
+			// Remove the listener on close
+			document.removeEventListener("click", this._ifClickOutside);
+		}
+	}
+
+	ifClickOutside(event) {
+		const targetElement = document.getElementById('mw-popover-box');
+		let clickedElement = event.target;
+
+		do {
+			if (clickedElement === targetElement) {
+				// This is a click inside the popover, do nothing, just return
+				return false;
+			}
+			// Go up the DOM
+			clickedElement = clickedElement.parentNode;
+		} while (clickedElement);
+
+		// This is an outside click
+		this.toggle();
 	}
 
 	view() {
 		return this.showing ? m(
 			'.mw-popover',
-			[m('.card-deck.mw-popover-box',
-				[m('.card.mw-shadow', [
+			[m('.card-deck.mw-popover-container',
+				[m('.card.mw-shadow', {id: 'mw-popover-box'}, [
 					m('.card-header.text-left.bg-info.text-white', this._content.title),
 					m('.card-body.text-left', this._content.view())
 				])]
